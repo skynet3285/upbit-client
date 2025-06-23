06220c47f9c7d5bf0b849efe2d18edfabd9d8d79
dots <skynet3285@gmail.com>
2025-06-23 19:15:31 +0900

    fix: correct Upbit ticker API response handling in upbit-ticker.repository.ts

--- dist/repository/upbit-ticker.repository.d.ts
+++ dist/repository/upbit-ticker.repository.d.ts
@@ -8,9 +8,9 @@ export declare class UpbitTickerRepository {
      * 업비트에서 요청 당시 종목의 스냅샷을 반환합니다.
      *
      * @param markets - 반점으로 구분되는 종목 코드 (ex. KRW-BTC, BTC-ETH)
-     * @returns {UpbitTicker} 종목 티커
+     * @returns {UpbitTicker[]} 종목 티커 정보 배열
      *
      * @see https://docs.upbit.com/kr/reference/ticker%ED%98%84%EC%9E%AC%EA%B0%80-%EC%A0%95%EB%B3%B4
      */
-    getTicker(markets: string): Promise<UpbitTicker>;
+    getTicker(markets: string): Promise<UpbitTicker[]>;
 }
